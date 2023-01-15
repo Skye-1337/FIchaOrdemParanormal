bee750cf0515a88f78dea9e1c71da4fc964534ed
Mystery <98327564+Mystery-1337@users.noreply.github.com>
2023-01-15 09:54:27 -0800

    Security features

--- Database.cs
+++ Database.cs
@@ -12,11 +12,6 @@ namespace FIchaOrdemParanormal
         public static void Connect()
         {
 
-            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://MASTER:Ficha_Ordem_MASTER@fichaordemparanormal.ezpfs2p.mongodb.net/?retryWrites=true&w=majority");
-            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            var client = new MongoClient(settings);
-            var database = client.GetDatabase("FichaOrdemParanormal");
-
         }
         public static void Disconnect()
         {
